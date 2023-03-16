9af7c1c6fac57f2f86c5a48a12ee444874434220
Or Dvir <or_dvir@hotmail.com>
2023-03-16 15:06:14 +0100

    created basis for event occurrences screen

--- app/src/main/java/com/hotmail/or_dvir/tracks/ui/eventOccurrenceScreen/EventOccurrencesScreen.kt
+++ app/src/main/java/com/hotmail/or_dvir/tracks/ui/eventOccurrenceScreen/EventOccurrencesScreen.kt
@@ -1,18 +1,111 @@
 package com.hotmail.or_dvir.tracks.ui.eventOccurrenceScreen
 
-import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getScreenModel
+import cafe.adriel.voyager.navigator.LocalNavigator
+import com.example.tracks.R
+import com.hotmail.or_dvir.tracks.collectAsStateLifecycleAware
+import com.hotmail.or_dvir.tracks.models.EventOccurrence
+import com.hotmail.or_dvir.tracks.models.TrackedEvent
+import com.hotmail.or_dvir.tracks.ui.eventOccurrenceScreen.EventOccurrencesViewModel.UserEvent.OnCreateNewOccurrence
 
 data class EventOccurrenceScreen(val event: TrackedEvent) : Screen {
     @Composable
     override fun Content() {
         val viewModel =
             getScreenModel<EventOccurrencesViewModel, EventOccurrencesViewModel.Factory> {
-                it.create(eventId)
+                it.create(event.id)
             }
 
-//        add empty view
+        val navigator = LocalNavigator.current
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = { navigator?.pop() }) {
+                            Icon(
+                                contentDescription = stringResource(R.string.contentDescription_back),
+                                imageVector = Icons.Filled.ArrowBack
+                            )
+                        }
+                    },
+                    title = { Text(event.name) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { viewModel.onUserEvent(OnCreateNewOccurrence) }
+                ) {
+                    Icon(
+                        contentDescription = stringResource(R.string.contentDescription_addEventOccurrence),
+                        imageVector = Icons.Filled.Add
+                    )
+                }
+            }
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    //todo what are the "contentValues"? do i need this???
+                    .padding(it)
+            ) {
+                val eventOccurrences =
+                    viewModel.eventOccurrencesFlow.collectAsStateLifecycleAware(initial = emptyList()).value
+
+                if (eventOccurrences.isEmpty()) {
+                    EmptyContent()
+                } else {
+                    NonEmptyContent(eventOccurrences = eventOccurrences)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun EmptyContent() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(stringResource(R.string.eventOccurrences_emptyView))
+        }
+    }
+
+    @Composable
+    private fun NonEmptyContent(eventOccurrences: List<EventOccurrence>) {
+        // todo
+        //  action bar title
+        //  FAB to add new occurrence
+        //  swipe to delete with confirmation dialog
+    }
+
+    @Composable
+    private fun EventOccurrenceRow() {
+        // todo
+        //  start date
+        //      edit feature
+        //  end date
+        //      edit feature
+        //  note
+        //      edit feature
     }
 }
--- app/src/main/java/com/hotmail/or_dvir/tracks/ui/eventOccurrenceScreen/EventOccurrencesViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/tracks/ui/eventOccurrenceScreen/EventOccurrencesViewModel.kt
@@ -3,29 +3,34 @@ package com.hotmail.or_dvir.tracks.ui.eventOccurrenceScreen
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.hilt.ScreenModelFactory
 import com.hotmail.or_dvir.tracks.database.repositories.EventOccurrencesRepository
+import com.hotmail.or_dvir.tracks.ui.eventOccurrenceScreen.EventOccurrencesViewModel.UserEvent.OnCreateNewOccurrence
+import com.hotmail.or_dvir.tracks.ui.eventOccurrenceScreen.EventOccurrencesViewModel.UserEvent.OnDeleteOccurrence
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-//@HiltViewModel
 class EventOccurrencesViewModel @AssistedInject constructor(
     @Assisted
     private val eventId: Int,
     private val repo: EventOccurrencesRepository
 ) : ScreenModel {
-//) : ViewModel() {
-
-//    look into this:
-//    https://github.com/adrielcafe/voyager/blob/main/samples/android/src/main/java/cafe/adriel/voyager/sample/hiltIntegration/HiltModule.kt
-//    i want to inject repository from Hilt, and eventId from voyager library using ScreenModel!
-//    according to https://voyager.adriel.cafe/screenmodel/hilt-integration,
-//    hilt integration should work without annotating @HiltViewModel
-//    does it just work????
-//    if not, HOW?!?!?!?!??!
-//    can i use saved handle state somehow???
-//    https://github.com/adrielcafe/voyager/blob/main/samples/android/src/main/java/cafe/adriel/voyager/sample/hiltIntegration/HiltListViewModel.kt
 
     val eventOccurrencesFlow = repo.getAllByStartDateDesc(eventId)
+
+    fun onUserEvent(event: UserEvent) {
+        when (event) {
+            is OnCreateNewOccurrence -> onCreateNewOccurrence()
+            is OnDeleteOccurrence -> onDeleteOccurrence()
+
+        }
+    }
+
+    private fun onCreateNewOccurrence() {
+        // todo
+    }
+
+    private fun onDeleteOccurrence() {
+        // todo
+    }
 
     @dagger.assisted.AssistedFactory
     interface Factory : ScreenModelFactory {
